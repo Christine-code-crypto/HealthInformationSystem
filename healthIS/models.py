eac1068c5e8bccf7dd882d90a0b92c407028a0cd
Christine Kerubo <82275560+Christine-code-crypto@users.noreply.github.com>
2025-04-26 22:37:33 +0300

    added models to the models.py file

--- healthIS/models.py
+++ healthIS/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class HealthProgram(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Client(models.Model):
+    GENDER_CHOICES = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other')
+    ]
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    dob = models.DateField()
+    email = models.EmailField(unique=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    contact_info = models.CharField(max_length=255)
+    #enrolled_programs = models.ManyToManyField(HealthProgram, related_name='clients', blank=True)
+    date_registered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class Enrollment(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    program = models.ForeignKey(HealthProgram, on_delete=models.CASCADE)
+    enrollment_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('client', 'program') # Ensure a client can't be enrolled in the same program twice
+
+    def __str__(self):
+        return f"{self.client.first_name} enrolled in {self.program.name}"
+
