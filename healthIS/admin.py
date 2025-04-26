532a513cba9e08484f72d4770fa9e2f8617f8d2f
Christine Kerubo <82275560+Christine-code-crypto@users.noreply.github.com>
2025-04-26 23:33:09 +0300

    registered models in admin.py file

--- healthIS/admin.py
+++ healthIS/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Client, HealthProgram,Enrollment
 
-# Register your models here.
+
+admin.site.register(Client)
+admin.site.register(HealthProgram)
+admin.site.register(Enrollment)
+
